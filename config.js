c502ed7f686d5c0f2877f645c9d7c4a7322b0f89
KazukiChat <109554024+pokemongg@users.noreply.github.com>
2022-08-13 15:09:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6285234107894','6285234107894','6285234107894','6285234107894']
+global.premium = ['6285234107894']
+global.packname = 'Kazuki Ganz'
 global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.sessionName = 'Whatssap'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
